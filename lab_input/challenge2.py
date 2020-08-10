eb28bdd337a62ce4a77cab3f7bf1c0b65387cb36
dzheng24 <zhengdav@msu.edu>
2020-08-10 17:25:26 -0400

    finished lab 8

---
+++ lab_input/challenge2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+name = input("What is your name? ")
+day = input("What day is today? ")
+print(f"Hello, {name}! Happy {day}!")
+
---
+++ lab_input/iptaker02.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+name = input("What is yo name?")
+print("Hello " + name)
+print("Let me get lunch early")
+        
+
+
+
