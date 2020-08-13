c925413f1ea825efdc834cf0173c497072a3d6fa
dzheng24 <zhengdav@msu.edu>
2020-08-12 21:23:27 -0400

    fixed pylint errors

--- flowchart_challenge/engineering_flowchart.py
+++ flowchart_challenge/engineering_flowchart.py
@@ -1,53 +1,57 @@
 #!/usr/bin/env python3
 
+"""
+Engineering Flowchart Practice Using Python Functions
+"""
 import sys
 
-# function to quit program if user presses "q"
-def handleQuit(input):
-    if input == "q":
+MESSAGE = "Enter yes or no:\n"
+
+def handle_quit(string):
+    """exit the program if input is q"""
+    if string == "q":
         print("Good bye")
         sys.exit(0)
 
-# entrypoint
 def main():
+    """calling the is_moving function based on user input"""
     print("Welcome to Engineering Flowchart. Press q at anytime to exit.\n\n")
-    isMoving = input("Does it move?\n")
-    handleIsMoving(isMoving)
+    is_moving = input("Does it move?\n")
+    handle_is_moving(is_moving)
 
-# function that handles user's initial response  
-def handleIsMoving(str):
-    handleQuit(str)
-    if str == "no":
-        isShouldNotMoving = input("Should it?\n")
-        handleShouldNotMoving(isShouldNotMoving)
-    elif str == "yes":
-        isShouldMoving = input("Should it?\n")
-        handleShouldMoving(isShouldMoving)
+def handle_is_moving(string):
+    """function that handles user's initial response"""
+    handle_quit(string)
+    if string == "no":
+        is_should_not_moving = input("Should it?\n")
+        handle_should_not_moving(is_should_not_moving)
+    elif string == "yes":
+        is_should_moving = input("Should it?\n")
+        handle_should_moving(is_should_moving)
     else:
-        answer = input("Enter yes or no:\n ")
-        handleIsMoving(answer)
+        answer = input(MESSAGE)
+        handle_is_moving(answer)
 
-# function that handles user's response when it is not supposed to move 
-def handleShouldNotMoving(str):
-    handleQuit(str)
-    if str == "no":
+def handle_should_not_moving(string):
+    """function that handles user's response when it is not supposed to move"""
+    handle_quit(string)
+    if string == "no":
         print("No problem")
-    elif str == "yes":
+    elif string == "yes":
         print("Try some WD-40")
-    else: 
-        answer = input("Enter yes or no:\n ")
-        handleShouldNotMoving(answer)
+    else:
+        answer = input(MESSAGE)
+        handle_should_not_moving(answer)
 
-# function that hanldes user's response when it is supposed to move 
-def handleShouldMoving(str):
-    handleQuit(str)
-    if str == "yes":
+def handle_should_moving(string):
+    """function that handles user's response when it is supposed to move"""
+    handle_quit(string)
+    if string == "yes":
         print("No problem")
-    elif str == "no":
+    elif string == "no":
         print("Put some duct tape on it")
     else:
-        answer = input("ENter yes or no:\n ")
-        handleShouldMoving(answer)
+        answer = input(MESSAGE)
+        handle_should_moving(answer)
 
 main()
-
