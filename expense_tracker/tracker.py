3e0fc0a141a281335b52a5825865b309b096fa36
David Zheng <zhengdav@msu.edu>
2020-08-20 18:18:12 -0700

    added skipped edit mode

--- expense_tracker/tracker.py
+++ expense_tracker/tracker.py
@@ -18,6 +18,7 @@ def init():
     month = input("What month would you like to track the expense?\n")
     dest_filename = month + ".xlsx"
     wb.save(filename=dest_filename)
+    edit_tracker(dest_filename)
 
 
 def open_prev():
@@ -40,20 +41,23 @@ def print_rows(sheet):
 
 def edit_tracker(doc_name):
     print("You are in editing mode of file: " + doc_name)
+    user_input = input("Enter \"yes\" to continue edit, hit enter to skip:\n")
+
     workbook = load_workbook(filename=doc_name)
     sheet = workbook.active
     sheet["A1"] = "Day"
     sheet["B1"] = "Money Spent"
     sheet["C1"] = "Description"
 
-    expense_date = input("What day would you like to add? Enter a number:\n")
-    expense_amount = input("Amount spent:\n")
-    expense_description = input("What did you spent it on? Enter here:\n")
+    if user_input.lower().startswith('y'):
+        expense_date = input("What day would you like to add? Enter a number:\n")
+        expense_amount = input("Amount spent:\n")
+        expense_description = input("What did you spent it on? Enter here:\n")
 
-    new_row = sheet.max_row + 1
-    sheet.cell(column=1, row=new_row, value=expense_date)
-    sheet.cell(column=2, row=new_row, value=expense_amount)
-    sheet.cell(column=3, row=new_row, value=expense_description)
+        new_row = sheet.max_row + 1
+        sheet.cell(column=1, row=new_row, value=expense_date)
+        sheet.cell(column=2, row=new_row, value=expense_amount)
+        sheet.cell(column=3, row=new_row, value=expense_description)
 
     print_rows(sheet)
 
